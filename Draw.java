4407756c7d2152bef485f3c02c5ba2370d870843
unknown <>
2016-11-01 17:31:48 +0800

    Draw

--- Draw.java
+++ Draw.java
@@ -1,39 +1,28 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 
-public class Draw extends JFrame
+import java.awt.*;
+import java.applet.*;
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Draw extends Applet
 {
-	private String s="Draw";
-    JFrame frame;
-	public Draw() 
-	{
-		frame = new JFrame("Draw oval,circle,arc and text");
-        frame.setSize(400,400);
-        frame.setVisible(true);
-	}
 	public void paint(Graphics g)
 	{
-		g.setColor(Color.red);
+		g.setColor(Color.RED);        //Õ÷‘≤
         g.drawOval(30,30,50,20);       
         g.fillOval(30,30,50,20);
 		
-		g.setColor(Color.green);       
-		g.drawOval(4, 4, width-8, height-8);
-		g.fillOval(4, 4, width-8, height-8);
+		g.setColor(Color.GREEN);      //‘≤
+		g.drawOval(4, 4, 8,8);
+		g.fillOval(4, 4, 8,8);
 		
-		g.setColor(Color.yellow);
+		g.setColor(Color.BLUE);       //‘≤ª°
 		g.drawArc(150,150,50,50,0,60); 
 		g.fillArc(210,210,50,50,0,-60);
-    }
-	public static void main(String[] args) 
-	{
-		Draw d=new Draw();
-        d.addWindowListener(
-        new WindowAdapter(){
-			public void windowClosing (WindowEvent e){
-				System.exit(0);
-				}
-			});
+		
+		g.setColor(Color.BLACK);
+		g.drawString("ctt",100,100);  //Œƒ±æ
+		super.paint(g);
     }
 }
